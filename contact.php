fea08b8d39ed99d07bd583fd0dbcef4f16dca3ab
syhnes <syhnes@gmail.com>
2024-01-16 21:51:55 +0100

    mise en place de formulaire de contact

--- contact.php
+++ contact.php
@@ -15,18 +15,17 @@
     <div class="container">
         <?php require_once(__DIR__ . '/header.php'); ?>
         <h1>Contactez nous</h1>
-        <form>
-            <div class="mb-3">
-                <label for="email" class="form-label">Email</label>
-                <input type="email" class="form-control" id="email" name="email" aria-describedby="email-help">
-                <div id="email-help" class="form-text">Nous ne revendrons pas votre email.</div>
-            </div>
-            <div class="mb-3">
-                <label for="message" class="form-label">Votre message</label>
-                <textarea class="form-control" placeholder="Exprimez vous" id="message" name="message"></textarea>
-            </div>
-            <button type="submit" class="btn btn-primary">Envoyer</button>
-        </form>
+            <form action="/submit_contact.php" method="GET">
+                <div>
+                    <label for="email">Email</label>
+                    <input type="email" name="email">
+                </div>
+                <div>
+                    <label for="message">Votre message</label>
+                    <textarea placeholder="Exprimez vous" name="message"></textarea>
+                </div>
+                <button type="submit">Envoyer</button>
+            </form>
         <br />
     </div>
     <?php require_once(__DIR__ . '/footer.php'); ?>
