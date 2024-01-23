60feee22b8209f6d6c420d2b30d59f437ba4fdaa
ASyhnes <syhnes@gmail.com>
2024-01-23 19:16:00 +0100

    MAJ cookies

--- contact.php
+++ contact.php
@@ -1,7 +1,7 @@
+<?php session_start(); ?>
+
 <!DOCTYPE html>
 <html>
-
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -9,31 +9,29 @@
     <title>Site de Recettes - Page d'accueil</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
-
-
 <body class="d-flex flex-column min-vh-100">
     <div class="container">
+
         <?php require_once(__DIR__ . '/header.php'); ?>
         <h1>Contactez nous</h1>
-            <form action="submit_contact.php" method="POST" enctype="multipart/form-data">
-                <div>
-                    <label for="email">Email</label>
-                    <input type="email" name="email">
-                </div>
-                <div>
-                    <label for="message">Votre message</label>
-                    <textarea placeholder="Exprimez vous" name="message"></textarea>
-                </div>
-                <div class="mb-3">
-                    <label for="screenshot" class="form-label">Votre capture d'écran</label>
-                    <input type="file" class="form-control" id="screenshot" name="screenshot" />
-                </div>
-                <button type="submit">Envoyer</button>
-            </form>
-        <br />
+        <form action="submit_contact.php" method="POST" enctype="multipart/form-data">
+            <div class="mb-3">
+                <label for="email" class="form-label">Email</label>
+                <input type="email" class="form-control" id="email" name="email" aria-describedby="email-help">
+                <div id="email-help" class="form-text">Nous ne revendrons pas votre email.</div>
+            </div>
+            <div class="mb-3">
+                <label for="message" class="form-label">Votre message</label>
+                <textarea class="form-control" placeholder="Exprimez vous" id="message" name="message"></textarea>
+            </div>
+            <div class="mb-3">
+                <label for="screenshot" class="form-label">Votre capture d'écran</label>
+                <input type="file" class="form-control" id="screenshot" name="screenshot" />
+            </div>
+            <button type="submit" class="btn btn-primary">Envoyer</button>
+        </form>
     </div>
+
     <?php require_once(__DIR__ . '/footer.php'); ?>
 </body>
-
-
 </html>
